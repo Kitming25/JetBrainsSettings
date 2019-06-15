a2a210753709f1704bf398438b81dd385cd3e7a7
lijieming <kitming25@gmail.com>
2019-06-15 10:16:06 +0800

    IU-2019.1.2 <DW@DW-PC Create baseRefactoring.xml, markdown.xml, IntelliLang.xml, DarkHope.icls, ide.general.xml, git.xml, _@user_Default.icls, eclipseCodeFormatter.xml, project.default.xml, diff.xml, Default copy.xml, JavaDev.icls, terminal.xml, Default.xml, find.xml, ui.lnf.xml, filetypes.xml, projectView.xml, keymap.xml, colors.scheme.xml, _@user_Material Oceanic.icls, notifications.xml, _@user_Darcula.icls, databaseDrivers.xml, Default.xml, laf.xml, parameter.hints.xml, Class.java, jdk.table.xml, path.macros.xml, Eclipse copy.xml, mavenRunHelper.xml, Ladies Night 2.xml, vcs.xml, editor.codeinsight.xml, grepConsole.xml, databaseSettings.xml, editor.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,9 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+ *
+ * @author kitming25
+ * @date ${DATE}
+*/
+public class ${NAME} {
+}
